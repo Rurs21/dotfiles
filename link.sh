79d697764966ad7ec250aefd58344e354590b82e
Rurs21 <charles-emile96@hotmail.com>
2021-01-31 14:25:14 -0500

    Fix Shebang & variable typo

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 
 is_hidden() {
     fchar=`basename $1 | cut -c1`
@@ -12,7 +12,7 @@ relink() {
     # Include hidden files and Exclude files starting with _
     for file in $1/{[^_],.[^.],..?}*; do
         if [ -f $file ]; then
-            ln -sf $FILE $2
+            ln -sf $file $2
         elif [ -d $file ]; then
             dir=`basename $file`
             dest=$2/$dir
