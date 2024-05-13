e6c3baec41bcbdb36070d904f1a998a20351615c
Rurs21 <rurs21@protonmail.com>
2024-05-13 10:41:50 -0500

    edit nvim config enhance lazy loading

--- configs/nvim/lua/plugins/harpoon.lua
+++ configs/nvim/lua/plugins/harpoon.lua
@@ -1,19 +1,35 @@
-local package = nil
-local function harpoon()
-	if package == nil then
-		package = require("harpoon")
-		package:setup()
-	end
-	return package
+local harpoon = nil
+
+local telescope_conf = nil
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = telescope_conf.file_previewer({}),
+        sorter = telescope_conf.generic_sorter({}),
+    }):find()
 end
 
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 	keys = {
-		{ "<C-e>", function() harpoon().ui:toggle_quick_menu(harpoon():list()) end, desc = "harpoon toggle quick menu" }, 
-		{ "<leader>a", function() harpoon():list():add() end, desc = "harpoon add to list" },
-	}
+		{ "<C-e>", function() toggle_telescope(harpoon:list()) end, desc = "Open harpoon window" },
+		{ "<leader>a", function() harpoon:list():add() end, desc = "harpoon add to list" },
+	},
+	config = function(_, opts)
+		harpoon = require("harpoon")
+		harpoon.setup(opts)
+		telescope_conf = require("telescope.config").values
+
+	end
 }
 
