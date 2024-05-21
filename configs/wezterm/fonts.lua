66fdc7b965113c48e5a49d3007f41f9a5033cae4
Rurs21 <rurs21@protonmail.com>
2024-05-21 12:30:46 -0400

    edit wezterm fonts feature & fallback

--- configs/wezterm/fonts.lua
+++ configs/wezterm/fonts.lua
@@ -1,29 +1,20 @@
 return {
-	{
-		family = 'Berkeley Mono Variable',
-		weight = 400,
-		harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-	},
+	{ family = 'Berkeley Mono', weight = 400 },
 	{ family = 'SF Mono', weight = 400 },
+	{ family = 'IBM Plex Mono', weight = 400 },
+	{ family = 'Monaspace Neon Var', weight = 400 },
 	{ family = 'Inconsolata', weight = 400 },
-	{
-		family = 'Monaspace Argon Var',
-		weight = 400,
-		harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-	},
-	{
-		family = 'IBM Plex Mono',
-		weight = 400,
-		harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-	},
-	{
-		family = 'Iosevka Term Extended',
-		weight = 'Medium',
-		harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-	},
-	'Fira Code',
-	'M+1Code Mono',
-	'Mononoki Mono',
-	'DengXian',
+	{ family = 'Iosevka Term Extended', weight = 'Medium' },
+	{ family = 'Iosevka Term Extended' },
+	{ family = 'Fira Code' },
+	--[[
+	--	CJK fallback
+	--	电脑, コンピューター, 컴퓨터
+	--]]
+	{ family = 'Noto Sans JP', weight = 'Medium', scale = 1 },
+	{ family = 'IBM Plex Sans KR', weight = 'Medium', scale = 1 },
+	{ family = 'Source Han Sans', weight = 'Medium', scale = 1 },
+	-- Symbols
+	{ family = 'Symbols Nerd Font Mono', scale = 0.95 }
 }
 
