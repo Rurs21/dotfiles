b7357dc2817303b9ab129ce0ce4dee0e037ec7ec
Rurs21 <me@rurs21.dev>
2024-05-22 11:45:10 -0500

    edit nvim config
    - organize plugins
    - add map to toggle dark/light background
    - edit lazy init

--- configs/nvim/lua/plugins/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-	'nvim-treesitter/playground',
-	{ 'tikhomirov/vim-glsl', ft = { "glsl" } }
-}
-
--- configs/nvim/lua/rurs/lazy.lua
+++ configs/nvim/lua/rurs/lazy.lua
@@ -1,19 +1,24 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+	vim.cmd('echo "Installing Lazy.nvim" | redraw')
+	local clone_cmd = {
+		"git", "clone", "--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git","--branch=stable",
 		lazypath,
-	})
+	}
+	vim.fn.system(clone_cmd)
+	vim.cmd('echo "Installed Lazy.nvim" | redraw')
 end
+-- prepend plugins data directory to vim runtime path
 vim.opt.rtp:prepend(lazypath)
 
 return require('lazy').setup('plugins', {
 	defaults = {
 		lazy = true
-	}
+	},
+	install = {
+		missing = false,
+		colorschme = { "default", "habamax" }
+	},
 })
 
