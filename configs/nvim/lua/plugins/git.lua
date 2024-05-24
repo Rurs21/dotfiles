745c23349e4cbd5dbb3bae0a000c9fda203d7a57
Rurs21 <me@rurs21.dev>
2024-05-24 13:09:13 -0400

    edit nvim git plugins config
    + add gisigns plugins
    + add condition function to load only if cwd have a .git repo

--- configs/nvim/lua/plugins/git.lua
+++ configs/nvim/lua/plugins/git.lua
@@ -1,7 +1,27 @@
+local function is_cwd_git_repo()
+	local path = vim.loop.cwd() .. "/.git"
+	local ok, err = vim.loop.fs_stat(path)
+	if not ok then
+		return false
+	else
+		return true
+	end
+end
+
 return {
-	'tpope/vim-fugitive',
-	keys = {
-		{ "<leader>gs", "<cmd>Git<cr>", desc = "Git" },
+	{
+		'tpope/vim-fugitive',
+		cond = is_cwd_git_repo,
+		keys = {
+			{ "<leader>gs", "<cmd>Git<cr>", desc = "Git" },
+		}
 	},
+
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufNew",
+		cond = is_cwd_git_repo,
+		opts = {},
+	}
 }
 
