50097ff5ac21080ff46c8e342baa2ede295f660a
Rurs21 <me@rurs21.dev>
2024-05-25 10:27:11 -0500

    add toggle for noob plugins

--- configs/nvim/lua/plugins/noob.lua
+++ configs/nvim/lua/plugins/noob.lua
@@ -1,16 +1,37 @@
+local commands = {
+	toogle = function() 
+		vim.cmd('Hardtime toogle')
+		require("precognition").toggle()
+		vim.cmd('echo "Noob plugin toogle !" | redraw')
+	end,
+}
+
+vim.api.nvim_create_user_command('Noob', function(args)
+	if commands[args.args] then
+		commands[args.args]()
+	end
+end,
+{
+	nargs = 1,
+	complete = function() return { 'toogle' } end,
+})
+
 return {
 	-- help establish good command workflow and habit
 	{
 		"m4xshen/hardtime.nvim",
-		event = 'VeryLazy',
+		cmd = "Hardtime",
 		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 		opts = {}
-	},	
-	
+	},
+
 	-- assists with discovering motions (show hints)
 	{
 		"tris203/precognition.nvim",
-		event = 'VeryLazy',
-		opts = {}
+		opts = {},
+		config = function(_,opts)
+		end
+
 	}
 }
+
