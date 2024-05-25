54e0cbc715a2594b8487ab7b05b3f2fc8eb6c69d
Rurs21 <me@rurs21.dev>
2024-05-25 07:42:54 -0500

    add nvim noob plugins

---
+++ configs/nvim/lua/plugins/noob.lua
@@ -0,0 +1,16 @@
+return {
+	-- help establish good command workflow and habit
+	{
+		"m4xshen/hardtime.nvim",
+		event = 'VeryLazy',
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		opts = {}
+	},	
+	
+	-- assists with discovering motions (show hints)
+	{
+		"tris203/precognition.nvim",
+		event = 'VeryLazy',
+		opts = {}
+	}
+}
