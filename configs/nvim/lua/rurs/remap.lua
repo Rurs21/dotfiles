4094d612750f838adac1d3d17c38caae4599afa5
Rurs21 <me@rurs21.dev>
2024-05-26 11:49:30 -0400

    clean nvim user config

--- configs/nvim/lua/rurs/remap.lua
+++ configs/nvim/lua/rurs/remap.lua
@@ -1,29 +1,27 @@
 -- set leader
 vim.g.mapleader = " "
-vim.opt.timeoutlen = 1500
-vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
+
+local keymap = vim.keymap
+
+keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 
 -- :)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- disable arrow keys
-vim.keymap.set({"n", "v", "i"}, "<Up>", "<nop>")
-vim.keymap.set({"n", "v", "i"}, "<Down>", "<nop>")
-vim.keymap.set({"n", "v", "i"}, "<Left>", "<nop>")
-vim.keymap.set({"n", "v", "i"}, "<Right>", "<nop>")
-
--- disable half page navigation
-vim.keymap.set({"n", "v", "i"}, "<C-d>", "<nop>")
-vim.keymap.set({"n", "v", "i"}, "<C-u>", "<nop>")
+keymap.set({"n", "v", "i"}, "<Up>", "<nop>")
+keymap.set({"n", "v", "i"}, "<Down>", "<nop>")
+keymap.set({"n", "v", "i"}, "<Left>", "<nop>")
+keymap.set({"n", "v", "i"}, "<Right>", "<nop>")
 
 -- move select line(s) up or down
-vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+keymap.set('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+keymap.set('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
 -- toggle background (light/dark)
-vim.keymap.set({"n", "v", "i"}, "<leader>tt", function()
+keymap.set("n", "<leader>tt", function()
 	if vim.o.background == "light" then
 		vim.opt.background = "dark"
 	else
